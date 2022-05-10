a72610387a7f06753a0fb759c7ec2109673e0966
Gabriel-Amar <amariutei.gabriel1@gmail.com>
2022-05-10 22:01:46 +0200

    vue: AppGrid con funzione cerca film

---
+++ src/components/AppGrid.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <input v-model="search" placeholder="Cerca un film" type="text">
+        <button @click="cercaFilm" type="button">Cerca Film</button>
+        
+            <div v-for="film in filmList" :key="film">
+                <p>{{'Titolo: '+film.title}}</p>
+                <p>{{'Lingua: '+film.original_language}}</p>
+                <p>{{'Voto: '+film.vote_average}}</p>
+            </div>
+        
+    </div>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+    name: "AppGrid",
+    data(){
+        return{
+            apiUrl: 'https://api.themoviedb.org/3/search/',
+            apiKey: "f9f88b1e607ce852eb8d91625dc47ba1",
+            filmList: [],
+            search:"",
+        }
+    },
+    methods:{
+        cercaFilm(){
+            const paramObj = {
+                params: {
+                    api_key: this.apiKey,
+                    query: this.search,
+                    language: "it-IT"
+                }
+            }
+                axios.get(this.apiUrl + "movie?", paramObj).then((res)=>{
+                    this.filmList = res.data.results;
+                })
+                .catch((err)=>{
+                    console.log(err)
+                })
+            }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+button{
+    color: black;
+}
+</style>
